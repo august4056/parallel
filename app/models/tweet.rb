72bbab17018bae6d417a694b525bee6b2ddcffe1
“Takahiro <august4056@gmail.com>
2025-09-26 00:40:50 +0900

    ふるいR:uby  on Railsを削除

--- app/models/tweet.rb
+++
@@ -1,7 +0,0 @@
-class Tweet < ApplicationRecord
-  validates :title, presence: true
-  
-  belongs_to :user
-
-  mount_uploader :image, ImageUploader
-end
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'tweets#index'
-
-  resources :tweets
-  resources :users
-end
